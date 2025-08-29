faa4e33268e4b990ffa4cc1d73d18e50acac1966
ahmetsenocak23 <ahmetsenocak1980@gmail.com>
2025-08-29 20:26:02 +0300

    ...

--- tests/test_new_password.py
+++ tests/test_new_password.py
@@ -8,6 +8,12 @@ def test_password_characters():
     for char in password:
         assert char in valid_characters
 
+def test_password_length():
+    """Oluşturulan şifrenin uzunluğunun belirtilen uzunlukla eşleştiğini test eder"""
+    length = 15  # Test etmek istediğiniz uzunluğu belirleyin
+    password = generate_password(length)
+    assert len(password) == length
+
 """
 Aşağıda önerilenlerden birini kullanarak başka bir test yazın. Alternatif olarak, kendi testinizi de oluşturabilirsiniz!
 Daha fazla test yazabilirseniz harika olur!
