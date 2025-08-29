19feea49ba13910dcb7ca07cd224f9011aff0770
tipacar <egetipacar@gmail.com>
2025-08-29 20:18:29 +0300

    ...

--- tests/test_new_password.py
+++ tests/test_new_password.py
@@ -8,6 +8,14 @@ def test_password_characters():
     for char in password:
         assert char in valid_characters
 
+def test_lenght():
+    password = generate_password(100)
+    assert len(password)==100
+
+def test_of_missary():
+    password1 = generate_password(100)
+    password2 = generate_password(100)
+    assert password1!=password2
 """
 Aşağıda önerilenlerden birini kullanarak başka bir test yazın. Alternatif olarak, kendi testinizi de oluşturabilirsiniz!
 Daha fazla test yazabilirseniz harika olur!
